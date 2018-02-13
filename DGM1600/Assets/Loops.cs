1af19a2ffe4ca843b6a2982eddd0e630c415eb70
Unknown <binghamstudent@live.com>
2018-02-13 14:26:48 -0700

    Deleted extra info and started Loop project

---
+++ DGM1600/Assets/Loops.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour {
+public string[] Players;
+public int[] Scores;
+
+	void Start () {
+		foreach(var player in Players);
+		print(Players);
+	
+		foreach(var score in Scores);
+		print(Scores);
+	}
+}
