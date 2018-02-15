11acce2854b100a8518cd728796cc933df24d987
Unknown <binghamstudent@live.com>
2018-02-15 12:24:52 -0700

    Explained lists

--- DGM1600/Assets/Scripts/Lists.cs
+++ DGM1600/Assets/Scripts/Lists.cs
@@ -7,6 +7,9 @@ public class Lists : MonoBehaviour {
 public Incredibles Incredibles;
 	public Text AddMember;
 private void OnMouseDown()
+//Lists are similar to loops but can be manipulated in game by the player. You could use lists or loops depending on what the situation
+//calls for. A good example of loops for me would be when you start a new game of zelda and they give you 3 game slots to make a new character
+//Loops will limit the gamer to the options the programer set, when lists would give you more options and flexibility as the gamer.
 {
 	
 	Incredibles.FamilyList.Add(AddMember.text);
