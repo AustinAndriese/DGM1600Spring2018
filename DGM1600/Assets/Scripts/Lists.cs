b5a4e345c580d81eedc7a7536ec9a7a6ad61d96d
Unknown <binghamstudent@live.com>
2018-02-15 12:18:15 -0700

    Explained loops and arrays, Also started lists

---
+++ DGM1600/Assets/Scripts/Lists.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lists : MonoBehaviour {
+public Incredibles Incredibles;
+	public Text AddMember;
+private void OnMouseDown()
+{
+	
+	Incredibles.FamilyList.Add(AddMember.text);
+
+		Incredibles.FamilyList.Add(AddMember.text);
+		
+
+		
+	}
+}
---
+++ DGM1600/Assets/Scripts/Loops.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour {
+public Player[] Players;
+public int[] HighScores;
+
+//Arrays are features where you add [] after int, string, etc. What this really does is open up slots in unity for you to add more
+//than one item to print. This could be used to keep scores, players, etc in the database. These are primarily used for a set number
+//and are meant for items that aren't changeable mid game. 
+
+//Loops are primarily used to shorten your code. Used well with arrays, they can pretty much grab a set group of arrays and display them.
+//Instead of manually listing each line of code, which could be hundreds of lines of code; you can use a loop to pretty much select the
+//preferred lines: example: Select line 1-100. Etc
+	void Start () {
+		foreach(var player in Players)
+		{
+		print(player.PlayerName);
+		print(player.Score);
+		print(player.Health);
+		}
+
+		foreach(var score in HighScores)
+	{
+		if(score >= 1000)
+		{
+		print(score);
+		}
+	}
+	}
+}
