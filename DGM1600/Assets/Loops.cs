3000f64f6af0d965f20487cf8232850d543f9777
Unknown <binghamstudent@live.com>
2018-02-15 10:43:50 -0700

    Fixed Loop problems

--- DGM1600/Assets/Loops.cs
+++ DGM1600/Assets/Loops.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Loops : MonoBehaviour {
-public string[] Players;
-public int[] Scores;
+public Player[] Players;
+public int[] HighScores;
 
 	void Start () {
-		foreach(var player in Players);
-		print(Players);
-	
-		foreach(var score in Scores);
-		print(Scores);
+		foreach(var player in Players)
+		{
+		print(player.PlayerName);
+		print(player.Score);
+		print(player.Health);
+		}
+
+		foreach(var score in HighScores)
+	{
+		if(score >= 1000)
+		{
+		print(score);
+		}
+	}
 	}
 }
