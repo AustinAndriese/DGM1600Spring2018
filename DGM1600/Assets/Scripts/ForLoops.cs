0b108ecaa5f0f86b9275585083c1c2bd459be3c6
Unknown <binghamstudent@live.com>
2018-02-27 12:10:28 -0700

    Worked on For Loops and Void Functions

---
+++ DGM1600/Assets/Scripts/ForLoops.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public string[] Keys;
+
+public class ForLoops : MonoBehaviour {
+	void Start () {
+		for (int i = 0; i < Keys.Length; i++)
+		
+		if (Keys[i] == "Add Ammo")
+		{
+			print(Keys[i]);
+		}
+		
+	}
+
+}
