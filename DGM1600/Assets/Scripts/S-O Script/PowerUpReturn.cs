7b639670fbc9e006271f7e6b81e5138fd381ce2a
Unknown <binghamstudent@live.com>
2018-03-08 11:23:44 -0700

    Started calculator using return function

---
+++ DGM1600/Assets/Scripts/S-O Script/PowerUpReturn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpReturn : MonoBehaviour {
+
+//increases its power levels every time you hit it.
+
+public Player CurrentPlayer;
+
+public int PowerLevel = 10;
+
+public int PowerLevelIncrease = 2;
+
+public int IncreasePowerLevel ()
+{
+	return PowerLevel *= PowerLevelIncrease;
+
+}
+
+private void OnMouseDown()
+{
+	CurrentPlayer.Health = IncreasePowerLevel();
+}
+
+
+}
