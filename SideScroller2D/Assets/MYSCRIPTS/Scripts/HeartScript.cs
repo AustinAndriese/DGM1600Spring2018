011873ca6cc31e8c020cafa684e3337d8ef57473
Unknown <binghamstudent@live.com>
2018-04-29 18:24:37 -0600

    Added Main screen and pause menu and changed character sprite

---
+++ SideScroller2D/Assets/MYSCRIPTS/Scripts/HeartScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartScript : MonoBehaviour
+{
+
+    public Sprite[] HeartSprites;
+
+    public Image HeartUI;
+
+    private Character player;
+
+    void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Character>();
+    }
+
+    void Update ()
+    {
+        HeartUI.sprite = HeartSprites[player.Health];
+    }
+}
